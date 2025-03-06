b92ee8f8b219f0e1256ab0bcb4524b0663774970
luzhe <2323811462@qq.com>
2025-03-06 11:27:52 +0800

    fix(新增查询当前日期前后N天的工作日): 更新版本至 1.0.1

--- src/WorkingDayChecker.php
+++ src/WorkingDayChecker.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace LaravelTools\WorkingDayChecker;
+
+use Carbon\Carbon;
 use LaravelTools\WorkingDayChecker\Exceptions\WorkingDayCheckerException;
 
 class WorkingDayChecker
@@ -28,14 +30,45 @@ class WorkingDayChecker
     }
 
     /**
-     * Get the next working day after the given date.
-     *
-     * @param string $date
-     * @return string
+     * 获取下个工作日，也可以获取指定日期前后几天的工作日
+     * @param $date
+     * @param $dayNum
+     * @return mixed
+     * @throws WorkingDayCheckerException
      */
-    public function getNextWorkingDay($date)
+    public function getNextWorkingDay($date, $dayNum = 1)
     {
         // 这里实现获取下一个工作日的逻辑
+        $this->isValidDate($date);
+
+        $getAllDates = $this->getAllDates();
+
+        $dateTime = strtotime($date);
+        $year = date('Y', $dateTime);
+        $month = date('m', $dateTime);
+        $day = date('d', $dateTime);
+        $getHoliday = getHoliday();
+
+        $dates = [];
+        for ($i = 1; $i <= 100; $i++) {
+            if (count($dates) >= abs($dayNum)) {
+                break;
+            }
+            $date = Carbon::create($year, $month, $day, 1, 0, 0);
+            if ($dayNum > 0) {
+                $future = $date->addDays($i);
+            } else {
+                $future = $date->subDays($i);
+            }
+
+            $futureDate = $future->format('Ymd');
+
+            if (isset($getHoliday[$futureDate]['workday']) && $getHoliday[$futureDate]['workday'] === 1) {
+                $dates[] = $future->format('Y-m-d');
+            }
+        }
+
+        return end($dates);
     }
 
     /**
@@ -51,8 +84,8 @@ class WorkingDayChecker
 
     public function getAllDates()
     {
-        $current_year_file_path = public_path() . 'holiday.txt';
-        $next_year_file_path    = public_path() . 'holiday_next.txt';
+        $current_year_file_path = $_SERVER['DOCUMENT_ROOT'] . 'holiday.txt';
+        $next_year_file_path = $_SERVER['DOCUMENT_ROOT'] . 'holiday_next.txt';
         if (date('m') === '03' && file_exists($next_year_file_path)) {
             // 删除 holiday.txt 文件 ，3月份的时候应该不会再用到去年的数据了，则把去年的数据删除，重命名holiday_next的文件为holiday
             @unlink($current_year_file_path);
@@ -79,20 +112,21 @@ class WorkingDayChecker
         $current_year_holidays = [];
         // 上面已经写入好文件内容，那么下面就进行读取文件内容
         if (file_exists($current_year_file_path)) {
-            $current_year_holidays = json_decode(file_get_contents($current_year_file_path),true)['data']['list'];
+            $current_year_holidays = json_decode(file_get_contents($current_year_file_path), true)['data']['list'];
 
         }
         $next_year_holidays = [];
         // 上面已经写入好文件内容，那么下面就进行读取文件内容
         if (file_exists($next_year_file_path)) {
-            $next_year_holidays = json_decode(file_get_contents($next_year_file_path),true)['data']['list'];
+            $next_year_holidays = json_decode(file_get_contents($next_year_file_path), true)['data']['list'];
         }
 
         $date = array_merge($current_year_holidays, $next_year_holidays);
         return array_column($date, null, 'date');
     }
 
-    public function isValidDate($dateString) {
+    public function isValidDate($dateString)
+    {
         $date = \DateTime::createFromFormat('Y-m-d', $dateString);
         if (!$date || $date->format('Y-m-d') !== $dateString) {
             throw new WorkingDayCheckerException('日期格式有误！'); // 抛出异常
